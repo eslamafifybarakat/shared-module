a1b6039d3f5fdd94ce3ddfa73c4e483c44fb4c6d
Eslam Afify Barakat <52158422+eslamafifybarakat@users.noreply.github.com>
2021-12-14 17:25:14 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    ProductsModule
+  ],
+  providers: [],
+  exports: [SharedModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
